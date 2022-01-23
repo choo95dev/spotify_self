b24a8c8b1430a3cc7990e1c58f362da7ff9f911d
Choo <choo.chenwei95dev@gmail.com>
2022-01-23 16:21:38 +0800

    Fix scope issues, extra 's'

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,33 +1,37 @@
 import SpotifyWebApi from "spotify-web-api-node";
 
 const scopes = [
-    "user-read-email",
-    "playlist-read-private",
-    "playlist-read-collabrative",
-    "user-read-email",
-    "streaming",
-    "user-read-private",
-    "user-library-read",
-    "user-top-read",
+    "ugc-image-upload",
     "user-read-playback-state",
     "user-modify-playback-state",
     "user-read-currently-playing",
-    "user-read-recently-played",
+    "user-read-private",
+    "user-read-email",
+    "user-follow-modify",
     "user-follow-read",
-    //"user-library-modify"
+    "user-library-modify",
+    "user-library-read",
+    "streaming",
+    "app-remote-control",
+    "user-read-playback-position",
+    "user-top-read",
+    "user-read-recently-played",
+    "playlist-modify-private",
+    "playlist-read-collaborative",
+    "playlist-read-private"
 ].join(',');
 
 const params = {
-    scopes:scopes
+  scope: scopes,
 };
 
 const queryParamString = new URLSearchParams(params);
 
-const LOGIN_URL=`https://accounts.spotify.com/authorize?${queryParamString.toString()}`;
+const LOGIN_URL = `https://accounts.spotify.com/authorize?${queryParamString.toString()}`;
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-    clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
 });
 
 export default spotifyApi;
